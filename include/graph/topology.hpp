ad004c39cea7780e07f0191103635c69e5a0e461
imhuwq <imhuwq@gmail.com>
2017-11-21 22:57:13 +0800

    (new)graph: typology order for directed graph

--- include/graph/topology.hpp
+++ include/graph/topology.hpp
@@ -6,6 +6,7 @@
 #define ALGS4_TOPOLOGY_HPP
 
 #include <vector>
+#include <deque>
 
 #include "graph.hpp"
 
@@ -13,6 +14,59 @@ using namespace std;
 using namespace graph;
 
 namespace graph {
+    class Typology {
+    private:
+        bool hasCycle;
+        vector<bool> marked;
+        deque<int> order;
+        vector<int> edgeTo;
+        vector<bool> onStack;
 
+        void DFS(DiGraph &pDiGraph, const int pV) {
+            marked[pV] = true;
+            onStack[pV] = true;
+
+            for (int lW: pDiGraph.ADJ(pV)) {
+                if (hasCycle) return;
+                if (!marked[lW]) {
+                    edgeTo[lW] = pV;
+                    DFS(pDiGraph, lW);
+                } else if (onStack[lW]) {
+                    hasCycle = true;
+                    return;
+                }
+            }
+
+            onStack[pV] = false;
+            order.push_front(pV);
+        }
+
+    public:
+        explicit Typology(DiGraph &pDiGraph) : marked(pDiGraph.V(), false), onStack(pDiGraph.V(), false), edgeTo(pDiGraph.V(), -1), hasCycle(false) {
+            for (int v = 0; v < pDiGraph.V(); v++) {
+                if (!marked[v]) DFS(pDiGraph, v);
+            }
+
+            if (hasCycle) order = deque<int>();
+        }
+
+        bool IsDAG() {
+            return !hasCycle;
+        }
+
+        vector<int> Order() {
+            return vector<int>(order.begin(), order.end());
+        }
+
+        bool PathTo(const int pTarget, vector<int> &rPath) {
+            if (hasCycle) return false;
+            deque<int> lDeque;
+            for (int x = pTarget; x != -1; x = edgeTo[x]) {
+                lDeque.push_front(x);
+            }
+            rPath = vector<int>(lDeque.begin(), lDeque.end());
+            return true;
+        }
+    };
 }
 #endif //ALGS4_TOPOLOGY_HPP
---
+++ src/graph/depth_first_directed_topolofy.cpp
@@ -0,0 +1,37 @@
+//
+// Created by john on 11/21/17.
+//
+
+#include <vector>
+#include <string>
+
+#include "graph/utils.hpp"
+#include "graph/topology.hpp"
+
+using namespace std;
+using namespace graph;
+
+int main(const int argc, const char **argv) {
+    pair<string, int> cmd = ParseCommandLine(argc, argv);
+    cout << "Test File: " << cmd.first << endl;
+    cout << "Source: " << cmd.second << endl;
+
+    InStream input(cmd.first);
+    DiGraph graph(input);
+
+    int source = cmd.second;
+    Typology topo(graph);
+
+    cout << graph.ToString() << endl;
+
+    cout << "Graph ";
+    if (topo.IsDAG()) cout << "NOT ";
+    cout << "Has Cycle" << endl;
+
+    if (topo.IsDAG()) {
+        for (int v:topo.Order()) {
+            cout << v << " ";
+        }
+    }
+    cout << endl;
+}
